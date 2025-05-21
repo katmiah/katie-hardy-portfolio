9a4adfb80e615919a6bbb9d97439727be8dd54c7
Katie Hardy <katiehardy@macbookpro.home>
2025-05-21 14:39:43 +0100

    fixed navbar, ready to be deployed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,31 @@
 function Navbar() {
   return (
-    <nav className="nav">
+    <header className="nav">
+      <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+      <label htmlFor="menu-toggle" className="menu-icon">
+        &#9776;
+      </label>
+
       <h1>Katie Hardy</h1>
-      <ul>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-        <li>
-          <a href="#work">Work</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-      </ul>
-    </nav>
+
+      <nav className="nav-links">
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+
+          <li>
+            <a href="#work">Work</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 export default Navbar;
